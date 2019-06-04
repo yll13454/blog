8987e0401837eaae0b56a56486d4ee4f7592dcae
yll13454 <451492304@qq.com>
2019-06-05 04:12:12 +0800

    初略完成版

--- slides/main.js
+++ slides/main.js
@@ -2,85 +2,69 @@ var $bts = $('.bt>button');
 var $images = $('.wrapper>img');
 var $wrapper = $('.wrapper');
 var current = 1
-var n = 0
-var m = 0
 var firstCopy = $images.eq(0).clone(true);
-var lastCopy = $images.eq($images.length-1).clone(true);
+var lastCopy = $images.eq($images.length - 1).clone(true);
 $wrapper.append(firstCopy);
 $wrapper.prepend(lastCopy);
-$wrapper.css({transform:'translateX(-600px)'})
+$wrapper.css({ transform: 'translateX(-600px)' })
 
-$('.bt').on('click','button',function(e){
+var setTime = setInterval(() => {
+    goToSlide(current + 1)
+}, 2000);
+
+$('.box').on('mouseenter',function(){
+    window.clearInterval(setTime)
+})
+
+$('.box').on('mouseleave',function(){
+    setTime = setInterval(() => {
+        goToSlide(current + 1)
+    }, 2000);
+})
+
+$bts.eq(0).on('click', function (params) {
+    goToSlide(current - 1)
+})
+
+$bts.eq(6).on('click', function (params) {
+    goToSlide(current + 1)
+})
+
+$('.bt').on('click', 'button', function (e) {
     var $button = $(e.currentTarget);
     var index = $button.index();
-    // console.log(current)
-    // console.log(index)
-    if(current === 1 && index === 0){
-        $wrapper.css({transform:'translateX(0)'}).one('transitionend',function(){
-            $wrapper.hide().offset();
-            $wrapper.css({
-                transform:'translateX(-'+($images.length)*600+'px)'
-            }).show()
-        })
-    }else if(current === 5 && index === 6){
-        $wrapper.css({transform:'translateX(-'+(index)*600+'px)'}).one('transitionend',function(){
-            $wrapper.hide().offset();
-            $wrapper.css({
-                transform:'translateX(-600px)'
-            }).show()
-        })
-    }else if( index === 0){
-        console.log('current:'+current)
-        if(current>0){
-            // console.log('1')
-            n = current
-            $wrapper.css({
-                transform:'translateX(-'+(n-1)*600+'px)'
-            })
-            n=n-1
-        }else if(n>1){
-            $wrapper.css({
-                transform:'translateX(-'+(n-1)*600+'px)'
-            })
-            n=n-1
-        }else{
-            $wrapper.css({transform:'translateX(-'+(index)*600+'px)'}).one('transitionend',function(){
-                $wrapper.hide().offset();
-                $wrapper.css({
-                    transform:'translateX(-3000px)'
-                }).show()
-            })
-            n = 5
-        }
-        console.log('n:'+n)
-    }else if(index === 6){
-        console.log('current:'+current)
-        if(current<6 ){
-            m = current
-            $wrapper.css({
-                transform:'translateX(-'+(m+1)*600+'px)'
-            })
-            m=m+1
-        }else if(m<5){
-            $wrapper.css({
-                transform:'translateX(-'+(m+1)*600+'px)'
-            })
-            m=m+1
-        }else{
-            console.log('3')
-            $wrapper.css({transform:'translateX(-3600px)'}).one('transitionend',function(){
-                $wrapper.hide().offset();
-                $wrapper.css({
-                    transform:'translateX(-600px)'
-                }).show()
-            })
-            m=1
-        }
-        console.log('m:'+m)
-    }else{
-        $wrapper.css({
-            transform:'translateX(-'+(index)*600+'px)'
-        })
-    }
-    current = index
+    console.log('current:', current)
+    console.log('index:', index)
+    if(index>0&&index<6){goToSlide(index)}
 })
+
+function goToSlide(index) {
+    if (index > 6) {
+        console.log('4')
+        index = 2
+    } else if (index < 0) {
+        index = 4
+    }
+    if (current === 1 && index === 0) {
+        $wrapper.css({ transform: 'translateX(0)' }).one('transitionend', function () {
+            $wrapper.hide().offset();
+            $wrapper.css({
+                transform: 'translateX(-' + ($images.length) * 600 + 'px)'
+            }).show()
+        })
+        current = index
+    } else if (current === 5 && index === 6) {
+        $wrapper.css({ transform: 'translateX(-' + (index) * 600 + 'px)' }).one('transitionend', function () {
+            $wrapper.hide().offset();
+            $wrapper.css({
+                transform: 'translateX(-600px)'
+            }).show()
+        })
+        current = index
+    } else{
+        $wrapper.css({
+            transform: 'translateX(-' + (index) * 600 + 'px)'
+        })
+        current = index
+    }
+}
