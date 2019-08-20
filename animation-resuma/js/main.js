f37f1d3d4b4a0e0dda252f2aa099c9c56691e0f2
yll13454 <451492304@qq.com>
2019-08-20 09:24:18 +0800

    会调地狱，重写

--- animation-resuma/js/main.js
+++ animation-resuma/js/main.js
@@ -5,24 +5,26 @@
     * 我就用代码来介绍吧
     * 首先准备一些样式
     */
-    
+
+/* 
+* 首先准备默认动画
+*/
 * {
     margin: 0;
     padding: 0;
     transition: all 1s;
 }
-
+/* 
+* 设置背景颜色
+*/
 body {
     height: 100vh;
     background-color: #eee;
     display: flex;
 }
-
-h2 {
-    text-align: center;
-    color: red;
-}
-
+/* 
+* 准备一张信纸
+*/
 .prewrapper {
     height: 100%;
     width: 50%;
@@ -30,7 +32,7 @@ h2 {
     padding: 16px;
     box-sizing: border-box;
     background-color: white;
-    overflow: scroll;
+    overflow: overlay;
     position: relative;
 }
 
@@ -51,26 +53,42 @@ h2 {
     left: 17px;
     width: 95%;
 }
-
 .prewrapper>.thickline>ol>li {
     display: block;
     border: 1px dotted red;
     margin-top: 26px;
 }
-.code {
-    font-size: 18px;
-    line-height: 28px;
+h2 {
+    text-align: center;
+    color: red;
+}
+/* 
+* 然后把代码移动过去，并适配合适
+*/
+.prewrapper .code {
     position: absolute;
     top: 61px;
     left: 12px;
+    bottom:auto;
+    font-size: 18px;
+    line-height: 28px;
 }
-
+/* 
+* 接着我们准备一张自我介绍的纸
+*/
 .paper{
+    box-sizing: border-box;
     flex: 1;
     height: 100%;
     width: 50%;
     overflow: overlay;
-}`
+    padding: 30px;
+    background-color:#DECEB3;
+}
+/* 
+* 现在我们打入自我介绍
+*/
+`
 
 
     var md = ` # 自我介绍
@@ -102,7 +120,20 @@ XXX 学校毕业
 - **Email** xxxxxxxx
 - **手机** xxxxxxx`
 
-
+var css2 =`
+/* 
+* 把自我介绍改的好看些
+*/
+.paper.content>h1,h3{
+    margin: 12px 0;
+}
+.paper.content>ol,li{
+    list-style: none;
+}
+/* 
+* 在加入markdown就完成了，这就是我的会动的简历
+*/
+`
 
     let view = document.querySelector('.prewrapper');
     let controller = {
@@ -111,10 +142,18 @@ XXX 学校毕业
         ol: null,
         codepage: null,
         codehtml: null,
-        init: function (view,css1,css2,md) {
+        css1:null,
+        css2:null,
+        css3:null,
+        md:null,
+        init: function (view,css1,css2,md,css3) {
             this.view = view;
             this.bindEvents();
-            this.writeCode(css1,css2,md)
+            this.css1 = css1;
+            this.css2 = css2;
+            this.css3 = css3;
+            let codehtml = document.querySelector('.paper .content');
+            this.writeCode(css1,css2,this.writeHtml(md,codehtml))
         },
         bindEvents: function () {
             let styleTag = document.querySelector('#styleTag');
@@ -126,38 +165,44 @@ XXX 学校毕业
             let codehtml = document.querySelector('.paper .content');
             this.codehtml = codehtml;
         },
-        writeCode: function (prefix, preCode,md) {
+        writeCode: function (prefix, preCode,fn) {
             let n = 0;
             let id = setInterval(() => {
                 n += 1;
                 this.codepage.innerHTML = prefix + preCode.substring(0, n);
                 this.codepage.innerHTML = Prism.highlight(prefix + preCode.substring(0, n), Prism.languages.css);
-                // this.styleTag.innerHTML = preCode.substring(0, n);
+                this.styleTag.innerHTML = prefix + preCode.substring(0, n);
                 this.codepage.scrollTop = this.codepage.scrollHeight;
-                if (n % 20 === 0) {
+                if(n>1100){
+                    this.view.scrollTop = this.view.scrollHeight;
+                }
+                if (n % 19 === 0) {
                     let li = document.createElement('li');
                     this.ol.append(li);
                 }
                 if (n > preCode.length) {
                     clearInterval(id);
-                    this.writeHtml(md,this.codehtml)
+                    fn();
                 }
             }, 10)
         },
-        writeHtml: function (result, preCode) {
+        writeHtml: function () {
             let n = 0;
             let id1 = setInterval(() => {
                 n += 1;
-                preCode.innerHTML = result.substring(0, n);
-                preCode.scrollTop = preCode.scrollHeight;
+                this.codehtml.innerHTML = this.md.substring(0, n);
+                this.codehtml.scrollTop = this.codehtml.scrollHeight;
                 // preCode.innerHTML = Prism.highlight(preCode.innerHTML,Prism.languages.md);
                 if (n > result.length) {
                     clearInterval(id1);
-                    document.querySelector('.paper').innerHTML = marked(result)
+                   this.writeCode(this.css2,this.css3,this.mdHtml);
                 }
             }, 10)
+        },
+        mdHtml:function(){
+            document.querySelector('.paper').innerHTML = marked(result);
         }
     }
-    controller.init(view,'',css,md);
+    controller.init(view,'',css,md,css2);
 }.call()
 
